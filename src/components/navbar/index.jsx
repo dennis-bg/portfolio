c381fc973400bff8b42c8629b6d0655dfbb5a57e
Dennis <dennis.galinsky@gmail.com>
2023-05-09 15:47:04 -0400

    higher order container

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,44 +3,47 @@ import React from "react";
 import styles from './styles.module.css'
 import { SmallLinkInSVG } from "../../svgs/linkedInSVG";
 import { SmallGithubSVG } from "../../svgs/gitHubSVG";
+import { Container } from "../common/container";
 
 export const Navbar = () => (
   <div className={styles.navBar}>
         
-    <div className={styles.container}>
-      <div className={styles.links}>
-        <h5>Dennis Galinsky</h5>
-        <a target="_blank" href="https://www.linkedin.com/in/dennis-galinsky-a1b406150/">
-          <SmallLinkInSVG/>
-        </a>
-        <a target="_blank" href="https://github.com/dennis-bg">
-          <SmallGithubSVG/>
-        </a>
-      </div>
-  
-  
-  <nav>
-      <ul>
-        <li>
-          <a id="projects" href="#project-section" >Projects</a>
-        </li>
-        <li>
-          <a id="skills" href="#skills-section" >Skills</a>
-        </li>
-        <li>
-          <a id="experience" href="#experience-section" >Experience</a>
-        </li>
-        <li>
-          <a id="contact" href="#contact-section" >Contact</a>
-        </li>
+    
+     <Container>
+          <div className={styles.links}>
+              <h5>Dennis Galinsky</h5>
+              <a target="_blank" href="https://www.linkedin.com/in/dennis-galinsky-a1b406150/">
+                <SmallLinkInSVG/>
+              </a>
+              <a target="_blank" href="https://github.com/dennis-bg">
+                <SmallGithubSVG/>
+              </a>
+            </div>
+        
+        
+        <nav>
+            <ul>
+              <li>
+                <a id="projects" href="#project-section" >Projects</a>
+              </li>
+              <li>
+                <a id="skills" href="#skills-section" >Skills</a>
+              </li>
+              <li>
+                <a id="experience" href="#experience-section" >Experience</a>
+              </li>
+              <li>
+                <a id="contact" href="#contact-section" >Contact</a>
+              </li>
 
-          
-      </ul>
+                
+            </ul>
 
-      
-  </nav>
+            
+        </nav>
+     </Container>
 
-    </div>
+   
  
 </div>
 )
