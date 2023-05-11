987567ad48fe65a6b6076ff1cc1366e7677fb036
Dennis <dennis.galinsky@gmail.com>
2023-05-11 11:35:31 -0400

    li click event

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -6,52 +6,62 @@ import { SmallGithubSVG } from "../../svgs/gitHubSVG";
 import { Container } from "../common/container";
 import { LinkButton } from "../common/container/linkButton";
 
-export const Navbar = () => (
-  <div className={styles.navBar}>
-        
-    
-     <Container>
+export const Navbar = () => {
 
-      <div className={styles.links}>
-      <h5>Dennis Galinsky</h5>
+  const handleLiClick = (e) => {
+    if(e.target.localName === 'a'){
+      return;
+    }
+    e.target.children[0].click();
+  }
 
-<nav>
-    <ul>
-      <li>
-        <a id="projects" href="#project-section" >Projects</a>
-      </li>
-      <li>
-        <a id="skills" href="#skills-section" >Skills</a>
-      </li>
-      <li>
-        <a id="experience" href="#experience-section" >Experience</a>
-      </li>
-      <li>
-        <a id="contact" href="#contact-section" >Contact</a>
-      </li>
-
-        
-    </ul>
-
-    
-</nav>
-      </div>
+  return (
+        <div className={styles.navBar}>
               
-        <div className={styles.links}>
-              <LinkButton
-                link={"https://www.linkedin.com/in/dennis-galinsky-a1b406150/"}
-                text={"Connect with me"}
-                svg={SmallLinkInSVG}
-              />
-              <LinkButton
-                link={"https://github.com/dennis-bg"}
-                text={"Visit my Github"}
-                svg={SmallGithubSVG}
-              />
-          </div>
-     </Container>
+          
+          <Container>
 
-   
- 
-</div>
-)
+            <div className={styles.links}>
+            <h5>Dennis Galinsky</h5>
+
+      <nav>
+          <ul>
+            <li onClick={e => handleLiClick(e)}>
+              <a id="projects" href="#project-section" >Projects</a>
+            </li>
+            <li onClick={e => handleLiClick(e)}>
+              <a id="skills" href="#skills-section" >Skills</a>
+            </li>
+            <li onClick={e => handleLiClick(e)}>
+              <a id="experience" href="#experience-section" >Experience</a>
+            </li>
+            <li onClick={e => handleLiClick(e)}>
+              <a id="contact" href="#contact-section" >Contact</a>
+            </li>
+
+              
+          </ul>
+
+          
+      </nav>
+            </div>
+                    
+              <div className={styles.links}>
+                    <LinkButton
+                      link={"https://www.linkedin.com/in/dennis-galinsky-a1b406150/"}
+                      text={"Connect with me"}
+                      svg={SmallLinkInSVG}
+                    />
+                    <LinkButton
+                      link={"https://github.com/dennis-bg"}
+                      text={"Visit my Github"}
+                      svg={SmallGithubSVG}
+                    />
+                </div>
+          </Container>
+
+        
+      
+      </div>
+    )
+}
