c11a625fb1b47c240486b8742cd33588f10e35ed
Dennis <dennis.galinsky@gmail.com>
2023-05-15 13:07:22 -0400

    skills with svg

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { Container } from "../common/container";
 import { MyTitle } from "../common/title";
-import styles from './styles.module.css';
 import { SkillsItem } from "./skillItem";
+import { DevActivitySVG } from '../../svgs/DevActivity';
+import { PairProgrammingSVG } from "../../svgs/PairProgramming";
+import { ProgrammingSVG } from "../../svgs/Programming";
+import styles from './styles.module.css';
 
 export const Skills = ({ skills }) => (
     <Container>
         <MyTitle title={"Skills"}/>
         <div className={styles.skillsContainer}>
-            {skills.map(item => 
-                <SkillsItem key={item.title} title={item.title} skillList={item.list}/>    
+            {skills.map(item => {
+                let svg = '';
+                switch(item.title){
+                    case 'Languages':
+                        svg = ProgrammingSVG;
+                        break;
+                    case 'Tools':
+                        svg = DevActivitySVG;
+                        break;
+                    case 'Coursework':
+                        svg = PairProgrammingSVG;
+                        break;
+                    default: break;
+                }
+                return <SkillsItem key={item.title} title={item.title} skillList={item.list} svg={svg}/>
+            }    
             )}
         </div>
     </Container>
--- src/components/skills/skillItem/index.jsx
+++ src/components/skills/skillItem/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from './styles.module.css';
 
-export const SkillsItem = ({ title, skillList }) => (
+export const SkillsItem = ({ title, skillList, svg }) => (
     <div className={styles.skillItem}>
+        {svg()}
         <h3>{title}</h3>
         <ul>
             {skillList.map(item => 
