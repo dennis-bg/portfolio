e7635016df8146d0b44618d50507f13eb0bad0de
Dennis <dennis.galinsky@gmail.com>
2023-05-16 13:26:08 -0400

    hero svg wip

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styles from './styles.module.css';
 import { Container } from "../common/container";
+import { FeelingProudSVG } from '../../svgs/FeelingProud';
+import styles from './styles.module.css';
 const resume = require('./Dennis Galinsky Resume.pdf');
 
 export const Hero  = () => {
@@ -9,23 +10,27 @@ export const Hero  = () => {
 
     return (
         <Container>
-            <div className={styles.hero}>
-            <div className={styles.h1Container}>
-              <h1>Dennis <span>Galinsky</span></h1>
+            <div className={styles.heroContainer}>
+                <div className={styles.left}>
+                  <div className={styles.hero}>
+                  <div className={styles.h1Container}>
+                    <h1>Dennis <span>Galinsky</span></h1>
+                  </div>
+                  <div className={styles.pContainer}>
+                    <p>Software Engineer in the New York City area, Interested in exciting opportunities in Frontend development but open to any positions!</p>
+                  </div>
+                  <div className={styles.viewContainer}>
+                    <a href="#project-section">View Portfolio <span>Below</span></a>
+                  </div>
+                  <div className={styles.downloadContainer}>
+                    <a href={resume} download="Dennis Galinsky Resume">Download Resume</a>
+                  </div>  
+                </div>
+                </div>
+                <div className={styles.right}>
+                  <FeelingProudSVG/>
+                </div>
             </div>
-            <div className={styles.pContainer}>
-              <p>Software Engineer in the New York City area, Interested in exciting opportunities in Frontend development but open to any positions!</p>
-            </div>
-            <div className={styles.viewContainer}>
-              <a href="#project-section">View Portfolio <span>Below</span></a>
-            </div>
-            <div className={styles.downloadContainer}>
-              <a href={resume} download="Dennis Galinsky Resume">Download Resume</a>
-            </div>
-            {/* <div className={styles.watermark2}>&ltDBG/&gt</div>
-            <div className={styles.watermark}>&ltDBG/&gt</div> */}
-  
-          </div>
         </Container>
     )
 }
